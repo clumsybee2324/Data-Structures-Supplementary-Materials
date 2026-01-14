3fbb0e9897d5783c8c7b5b7b40cf18b4df4d7c8d
Ayda <kafamkaristi05@gmail.com>
2026-01-15 01:50:41 +0300

    Singly Linked List add&remove

---
+++ singlylinkedlist2.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+    int data;
+    struct Node* next;
+}Node;
+
+typedef struct List{
+    Node* head;
+    int elemcount;
+}List;
+
+void createList(List* list){
+    list->elemcount=0;
+    list->head=NULL;
+}
+
+void addElement(List* list, int data){
+    Node* node=(Node*)malloc(sizeof(Node));
+    if(!node)return;
+
+    node->data=data;
+    node->next=list->head;
+    list->head=node;
+    list->elemcount++;
+}
+
+void removeElement(List* list){
+    Node* old=list->head;
+    list->head=old->next;
+    free(old);
+    list->elemcount--;
+}
+
+void addToPosition(List* list, int data, int position){
+    Node* cur=list->head;
+    Node* prev=NULL;
+
+    for(int i=0; i<position; i++){
+        prev=cur;
+        cur=cur->next;
+    }
+
+    Node* node=(Node*)malloc(sizeof(Node));
+    node->data=data;
+    node->next=cur->next;
+    prev->next=node; 
+    list->elemcount++;   
+}
+
+void removeFromPosition(List* list, int position){
+    Node* cur=list->head;
+    Node* prev=NULL;
+
+    for(int i=0; i<position; i++){
+        prev=cur;
+        cur=cur->next;
+    }
+
+    Node* old=cur;
+    prev->next=cur->next;
+    free(old);
+    list->elemcount--;
+}
+
+int main(){
+    List* mylist=(List*)malloc(sizeof(List));
+    createList(mylist);
+
+    addElement(mylist, 5);
+    addElement(mylist, 4);
+    addElement(mylist, 3);
+    addElement(mylist, 2);
+    addElement(mylist, 1);
+
+    removeElement(mylist);
+
+    addToPosition(mylist, 67, 3);
+
+    removeFromPosition(mylist, 2);
+
+    Node* current=mylist->head;
+    while (current !=NULL){
+        printf(" %d -> ", current->data);
+        current=current->next;
+    }
+    printf("finish!");
+    free(mylist);
+}
